b3fadd44e205093920384c9a1d7f04082304e576
poopoomachiene <guga_zanella@hotmail.com>
2016-12-15 23:51:01 -0500

    Add files via upload

--- Ok.js
+++ Ok.js
@@ -3,9 +3,4 @@ function play(){
        audio.play();
       }
 
-$('a[rel=popover]').popover({
-    html: true,
-    trigger: 'hover',
-    placement: 'bottom',
-    content: function(){return '<img src="'+$(this).data('img') + '" />';}
-});
+$('["popover"]').popover()
